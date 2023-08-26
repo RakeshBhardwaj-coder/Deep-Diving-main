17419a9fda65f38bb902df432291261c1f3ea9a5
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2023-08-26 20:25:16 +0530

    making UI

---
+++ Assets/MyAssets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public Slider loadingSlider;
+    public GameObject loadingPanel;
+    private void Start()
+    {
+        // Initialize volume slider
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 0.8f);
+    }
+
+    public void PlayButtonClicked()
+    {
+        // Show loading panel
+        loadingPanel.SetActive(true);
+
+        StartCoroutine(LoadGameScene());
+        
+    }
+    private IEnumerator LoadGameScene()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Game");
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f); // Limit to 0-1 range
+            loadingSlider.value = progress;
+            yield return null; // Wait for the next frame
+        }
+    }
+
+    public void OptionsButtonClicked()
+    {
+        // Handle options menu UI or settings
+        // You can enable/disable UI elements or open a settings panel
+    }
+
+    public void VolumeSliderChanged()
+    {
+        // Adjust the audio volume based on the slider value
+        float volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("Volume", volume);
+        AudioListener.volume = volume;
+    }
+
+    public void ExitButtonClicked()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+}
