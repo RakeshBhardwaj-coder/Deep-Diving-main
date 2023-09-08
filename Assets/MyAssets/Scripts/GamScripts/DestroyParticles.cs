ade981e5a98316cc1447d6600197b1e314b473f2
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2023-09-08 17:39:24 +0530

    nice look

--- Assets/MyAssets/Scripts/GamScripts/DestroyParticles.cs
+++ Assets/MyAssets/Scripts/GamScripts/DestroyParticles.cs
@@ -6,7 +6,7 @@ public class DestroyParticles : MonoBehaviour
 {
      public void DestoryTheParticles(GameObject particle, float duration)
         {
-            Destroy(particle, duration);
+           /* Destroy(particle, duration);*/
         }
     }
   
